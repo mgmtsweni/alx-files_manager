95240f6eb4338eb90a140fb9872f65f580e558d9
mgmtsweni <mtswenigift@gmail.com>
2023-06-02 00:36:20 +0200

    Submit

---
+++ utils/db.js
@@ -0,0 +1,38 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+
+const URL = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+    constructor() {
+      this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+      this.client.connect().then(() => {
+        this.db = this.client.db(`${DATABASE}`);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  
+    isAlive() {
+      return this.client.isConnected();
+    }
+  
+    async nbUsers() {
+      const users = this.db.collection('users');
+      const usersNum = await users.countDocuments();
+      return usersNum;
+    }
+  
+    async nbFiles() {
+      const files = this.db.collection('files');
+      const filesNum = await files.countDocuments();
+      return filesNum;
+    }
+  }
+  
+const dbClient = new DBClient();
+
+module.exports = dbClient;
